21be9681c7cb6f1bdc57d648bbe449b3a9f5372a
Jey <jey.michael@gmail.com>
2009-03-04 18:45:28 -0800

    Add default implementations for RINGER_ON RINGER_OFF keys.

--- phone/com/android/internal/policy/impl/PhoneWindow.java
+++ phone/com/android/internal/policy/impl/PhoneWindow.java
@@ -1233,6 +1233,29 @@ public class PhoneWindow extends Window implements MenuBuilder.Callback {
                 }
                 return true;
             }
+            case KeyEvent.KEYCODE_RINGER_ON:
+            case KeyEvent.KEYCODE_RINGER_OFF: {
+                AudioManager audioManager = (AudioManager) getContext().getSystemService(
+                        Context.AUDIO_SERVICE);
+                if (audioManager != null) {
+                    if (keyCode == KeyEvent.KEYCODE_RINGER_ON) {
+                        audioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+                    } else {
+                        // keyCode == KeyEvent.KEYCODE_RINGER_OFF
+                        int vibSetting = audioManager.getVibrateSetting(AudioManager.VIBRATE_TYPE_RINGER);
+                        int ringerMode = AudioManager.RINGER_MODE_SILENT;
+                        if (vibSetting == AudioManager.VIBRATE_SETTING_ON || vibSetting == AudioManager.VIBRATE_SETTING_ONLY_SILENT)
+                            ringerMode = AudioManager.RINGER_MODE_VIBRATE;
+
+                        audioManager.setRingerMode(ringerMode);
+                    }
+                    audioManager.adjustSuggestedStreamVolume(
+                            AudioManager.ADJUST_SAME,
+                            AudioManager.STREAM_RING,
+                            AudioManager.FLAG_SHOW_UI);
+                }
+                return true;
+            }
 
 
             case KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE:
